6ce06d997ddd933514ea366599bd37bd32b2f897
Yuliyan Andreev <habybbo@gmail.com>
2022-03-05 19:24:56 +0200

    Add project files.

---
+++ UnderwareStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnderwareStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
